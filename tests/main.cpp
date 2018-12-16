72b95dfb8ab0568425b395319ed3609560f2a941
Connor <c.mcg132@gmail.com>
2018-12-16 03:41:33 -0500

    removed pause in test executable

--- tests/main.cpp
+++ tests/main.cpp
@@ -4,22 +4,9 @@
 #include "gtest/gtest.h"
 #include "util/OUI_StringUtil.h"
 
-static void EXPECT_SSTREQ(const std::string& expected, const std::string& actual) {
-    EXPECT_STREQ(expected.c_str(), actual.c_str());
-}
-
-static void EXPECT_SSTRNE(const std::string& expected, const std::string& actual) {
-    EXPECT_STRNE(expected.c_str(), actual.c_str());
-}
-
-static void EXPECT_SSTREQ16(const std::u16string& expected, const std::u16string& actual) {
-    EXPECT_STREQ(convertUTF16toUTF8(expected).c_str(), convertUTF16toUTF8(actual).c_str());
-}
-
 // All test files are found by CMake!
 int main(int argc, char** argv) 
 { 
     testing::InitGoogleTest(&argc, argv); 
     RUN_ALL_TESTS(); 
-    std::getchar(); // keep console window open until Return keystroke
 }
